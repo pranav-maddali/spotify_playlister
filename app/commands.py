c02bce32be4b0da6514301f8c602e6abbe458f54
Pranav Maddali <60276842+pranav-maddali@users.noreply.github.com>
2021-06-29 17:06:09 -0600

    click command

--- app/commands.py
+++ app/commands.py
@@ -1,8 +1,10 @@
+import click
 from flask.cli import with_appcontext
 
 from app.extensions import db
 from app.models import User
 
+@click.command(name='create_tables')
 @with_appcontext
 def create_tables():
     db.create_all()
